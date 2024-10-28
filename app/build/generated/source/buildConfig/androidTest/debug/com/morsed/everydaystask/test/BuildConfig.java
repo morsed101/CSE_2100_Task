091a0f91ef3f77db5da5a171aac8dae68ee9bac3
morsed101 <mahabubmorsed44@gmail.com>
2024-10-28 17:22:33 +0600

    Add files via upload

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/morsed/everydaystask/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.morsed.everydaystask.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.katlegosetou.ontheball.test";
+  public static final String BUILD_TYPE = "debug";
+}
